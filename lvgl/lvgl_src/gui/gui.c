1a35360cdde9a7d0c25eb1265816b0f9d92ad339
YangMou <mr_yang_mou@163.com>
2022-02-24 11:15:41 +0800

    移植lvgl成功

--- lvgl/lvgl_src/gui/gui.c
+++ lvgl/lvgl_src/gui/gui.c
@@ -1,38 +1,46 @@
 #include "gui.h"
 
 
-static void event_handler(lv_event_t * e)
+static void create_status_bar( void )
 {
-    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t* status_bar = lv_obj_create( lv_layer_top() );
+    lv_obj_set_size( status_bar, LV_HOR_RES, LV_VER_RES/10 ); /*设置大小*/
+    lv_obj_align( status_bar, LV_ALIGN_TOP_MID, 0, 0); /*设置坐标*/
 
-    if(code == LV_EVENT_CLICKED) {
-        LV_LOG_USER("Clicked");
+    static lv_style_t style_status_bar;
+    lv_style_init( &style_status_bar );
+    lv_style_set_bg_color( &style_status_bar, lv_color_hex(0x00bfff) ); /*设置背景颜色*/
+
+    lv_obj_add_style( status_bar, &style_status_bar, LV_STATE_DEFAULT); /*默认style*/
+
+    static lv_ft_info_t info;
+
+    info.name = "AGENCYB.TTF";
+    info.weight = 100;
+    info.style = FT_FONT_STYLE_NORMAL;
+    info.mem = NULL;
+    if(!lv_ft_font_init(&info)) {
+        LV_LOG_ERROR("create failed.");
     }
-    else if(code == LV_EVENT_VALUE_CHANGED) {
-        LV_LOG_USER("Toggled");
-    }
+
+    /*Create style with the new font*/
+    static lv_style_t style;
+    lv_style_init(&style);
+    lv_style_set_text_font(&style, info.font);
+    lv_style_set_text_align(&style, LV_TEXT_ALIGN_CENTER);
+
+    /*Create a label with the new style*/
+    lv_obj_t * label = lv_label_create(lv_scr_act());
+    lv_obj_add_style(label, &style, 0);
+    lv_label_set_text(label, "Hello world\nI'm a font created with FreeType");
+    lv_obj_center(label);
+
+    return;
 }
+
 
 void gui(void)
 {
-    lv_obj_t * label;
-
-    lv_obj_t * btn1 = lv_btn_create(lv_scr_act());
-    lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
-    lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
-
-    label = lv_label_create(btn1);
-    lv_label_set_text(label, "Button");
-    lv_obj_center(label);
-
-    lv_obj_t * btn2 = lv_btn_create(lv_scr_act());
-    lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_ALL, NULL);
-    lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
-    lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
-    lv_obj_set_height(btn2, LV_SIZE_CONTENT);
-
-    label = lv_label_create(btn2);
-    lv_label_set_text(label, "Toggle");
-    lv_obj_center(label);
+    create_status_bar();
 }
 
